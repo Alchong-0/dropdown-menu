dbc5ce8cf28053d8db41929f76c6b4214f7ee95c
alexl <alexlchong@gmail.com>
2025-11-04 17:04:22 -0500

    Add dropdown menu on hover

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-const dropdownMenuElement = document.getElementById("menu");
+import "./styles.css";
+const dropdownMenuElement = document.getElementsByClassName("menu");
 
+function showDropdownMenu(e) {
+    for(const child of e.target.children) {
+        if(child.className == "option") {
+            child.style.display = "block";
+        }
+    }
+}
+
+function hideDropdownMenu(e) {
+    for(const child of e.target.children) {
+        if(child.className == "option") {
+            child.style.display = "none";
+        }
+    }
+}
+
+for(const elem of dropdownMenuElement) {
+    elem.addEventListener("mouseover", showDropdownMenu);
+    elem.addEventListener("mouseleave", hideDropdownMenu);
+}
